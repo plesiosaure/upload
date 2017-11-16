4a8490859c0a0d7ebb455edf6d492a73199f5355
plesiosaure <42@e-xode.com>
2017-11-16 17:43:41 +0100

    finished

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,30 +2,42 @@ var express = require('express');
 var router = express.Router();
 
 const multer = require('multer');
-const upload = multer({ dest: 'tmp/'});
+const upload = multer({ dest: 'tmp/', fileFilter:fileFilter});
 const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Upload de fichier' });
+  res.render('index', { title: 'Upload de fichiers multiples' });
 });
 
 
 router.post('/uploaddufichier', upload.array('monfichier', 4), function(req, res, next){
-
-req.files.forEach(function(image){
-console.log(image);
-if (image.size<(3*1024*1024) && image.mimetype == 'image/png'){
-//traitement du formulaire
-console.log('c\'est ok');
-//déplacement fichier du tmp à public/images
-fs.rename(image.path, 'public/images/' + image.originalname);
-} else {
-res.send('erreur dans le téléchargement du fichier');
-}
+  
+  req.files.forEach(function(image){
+ 
+    if (image.size<(3*1024)){
+    //traitement du formulaire
+  
+    //déplacement fichier du tmp à public/images
+      fs.rename(image.path, 'public/images/' + image.originalname, function(err){
+        if (err) {
+            res.send('problème durant le déplacement'); 
+        } 
+      }); 
+    } else {
+      return next(new Error('le fichier ' + image.originalname + ' est trop gros'));
+      //res.send('le fichier ' + image.originalname + ' est trop gros');
+    }
+  });
+  res.send('fichiers téléchargés avec succès');
 });
-res.send('fichier téléchargé avec succès');
-});
 
+function fileFilter(req, file, cb){
+  const extension = file.mimetype.split('/')[1];
+  if(extension !== 'png'){
+      return cb(new Error('le fichier ' + file.originalname + ' n\'est pas en png' ), false);
+  }
+  cb(null, true);
+};
 
 module.exports = router;
